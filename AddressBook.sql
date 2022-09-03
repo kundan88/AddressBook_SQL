dff8d421548ee1864d5550f5b3879b9fced9f7a3
Kundan <kundan kamble>
2022-09-02 23:03:34 -0800

    sort alphabetically

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,4 +1,3 @@
-
 ---UC1 Create AddressBook Database----
 CREATE DATABASE AddressBook
 
@@ -23,7 +22,10 @@ CREATE TABLE Addressbook
 
 INSERT INTO Addressbook VALUES('Kundan','Kamble','Colaba','Mumbai','Maharashtra',400005,7410741141,'kundan358@gmail.com')
 INSERT INTO Addressbook VALUES('Sunil','Kadam','Udgir','Latur','Maharashtra',413517,7410786644,'sunil@gmail.com')
+INSERT INTO Addressbook VALUES('Mahesh','surve','Ameer Peth','Hyd','TS',410002,7410786643,'mahesh@gmail.com')
+INSERT INTO Addressbook VALUES('Tushar','Swami','Bhalki','Bidar','Karnataka',413548,7410785544,'tushar@gmail.com')
 
+select * from Addressbook
 
 -----UC4 Edit Contact-----
 
@@ -33,6 +35,8 @@ UPDATE Addressbook SET Address = 'Cuffe parade' WHERE FirstName = 'Kundan'
 
 DELETE FROM Addressbook WHERE FirstName = 'Sunil'
 
+
+
 ------UC6 Retrieve Person From City or State-----------
 
 SELECT * FROM Addressbook WHERE City = 'Mumbai' or State = 'Maharashtra'
@@ -41,5 +45,8 @@ SELECT * FROM Addressbook WHERE City = 'Mumbai' or State = 'Maharashtra'
 
 SELECT COUNT(City) FROM Addressbook WHERE City = 'Mumbai' and State = 'Maharashtra'
 
+-----UC8 Sort By Alphabeticaly--------------
+
+SELECT * FROM Addressbook WHERE City = 'Mumbai' ORDER BY FirstName
 
 
