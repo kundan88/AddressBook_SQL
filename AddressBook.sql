7cf6789c4d7d8a65276e63a680e3f761e14ca6fe
Kundan <kundan kamble>
2022-09-02 22:52:48 -0800

    Create Table

--- AddressBook.sql
+++ AddressBook.sql
@@ -4,3 +4,17 @@ CREATE DATABASE AddressBook
 
 USE AddressBook
 
+----UC2 Create AddressBook Table---
+
+CREATE TABLE Addressbook
+(
+	ID INT IDENTITY(1,1) PRIMARY KEY,
+	FirstName VARCHAR(20) NOT NULL,
+	LastName VARCHAR(20) NOT NULL,
+	Address VARCHAR(100) NOT NULL,
+	City VARCHAR(20) NOT NULL,
+	State VARCHAR(20) NOT NULL,
+	Zip BIGINT NOT NULL,
+	PhoneNumber BIGINT NOT NULL,
+	Email VARCHAR(50) NOT NULL
+);
